e0e5933aca2ea114e8e0f057d3e6a0dcb3da3865
garvamel <garvamel@gmail.com>
2024-11-25 00:24:44 -0300

    Update getters.py with get_db(), get_records(), get_record_db_relation(), which retrieve fields for both db and field notes and their relationships for transcription.

--- getters.py
+++ getters.py
@@ -5,15 +5,16 @@ import pandas as pd
 class DataGetter:
     def __init__(self):
         if config.DATA_FILE.endswith(".xlsx"):
-            self.config_data = pd.read_excel(config.DATA_FILE, sheet_name=['Team', 'Database-Records'],keep_default_na=False)
+            self.setup_data = pd.read_excel(config.DATA_FILE, sheet_name=['Team', 'Database-Records'])
+
         else:
             raise("Wrong data file. Needs to be excel and in the format described in the README.")
 
 
     def get_team(self) -> dict:
         team = dict()
-        names = self.config_data["Team"]
-        names = names.replace('nan','')
+        names = self.setup_data["Team"]
+        names = names.fillna('')
         for name, apellido1, apellido2 in zip(names.Nombre, names.Apellido_1, names.Apellido_2):
             
             if apellido2 == '':
@@ -26,3 +27,25 @@ class DataGetter:
             team[key_initials] = full_name
 
         return team
+    
+    def get_db(self) -> list:
+        db_fields = self.setup_data["Database-Records"].columns.to_list()
+
+        return db_fields
+        
+    def get_records(self) -> list:
+        test_func = lambda x: x != ''
+        filtered_fields = list(filter(test_func, self.setup_data["Database-Records"].iloc[0].to_list()))
+
+        return filtered_fields
+    
+    def get_record_db_relation(self) -> dict:
+        
+        # Dict like {"DB Field": "Field_record_field"}
+        # No empty field record fields.
+
+        relations = self.setup_data["Database-Records"].dropna(axis=1, how='all') # remove all columns that have no data.
+        record_db = relations.to_dict(orient='records')[0] #record returns a list of dicts, so access the first one.
+        inverted_records = dict((v, k) for k, v in record_db.items())
+
+        return inverted_records
